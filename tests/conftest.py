8f346c7ec6558fb5172ecfce45c3ad8f8c9fbde5
andrechizh8 <andrechizh.ru@yandex.ru>
2022-12-05 23:53:23 +0300

    version 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,5 @@ from selene.support.shared import browser
 def browser_open():
     browser.config.window_width = 1080
     browser.config.window_height = 1920
-    browser.open('https://demoqa.com/automation-practice-form').driver.maximize_window()
+    browser.config.base_url = 'https://demoqa.com'
 
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -3,8 +3,10 @@ from selene.support.shared import browser
 from selene import be, have
 
 
+#
 def test_practice_form(browser_open):
     # Заполняем данные
+    browser.open('https://demoqa.com/automation-practice-form')
     browser.element('#firstName').should(be.blank).type('Andrew')
     browser.element('#lastName').should(be.blank).type('Chizh')
     browser.element('#userEmail').should(be.blank).type('andrechizh8@yandex.ru')
